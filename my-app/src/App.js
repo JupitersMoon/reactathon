db96fca9d4e37c9154a9d4c1ca5d3648c7a55633
144mdgross <144.mdgross@gmail.com>
2017-07-12 13:47:38 -0600

    renders wheel on page based off hardcoded array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SpinWheelComponent from './components/spinwheel'
+
+const SpinWheel = new SpinWheelComponent()
 
 class App extends Component {
+
+  componentDidMount() {
+    SpinWheel.drawRouletteWheel();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <SpinWheelComponent />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
